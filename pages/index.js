08463f06805657a1b5fec2fb3db9162cf059b8b6
r03eRt <morgadoluengo@gmail.com>
2022-03-18 01:27:42 +0100

    Building the header Component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -7,9 +8,8 @@ export default function Home() {
         <title>Hulu 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>Lets build Hulu 2.0</h1>
       {/** Header */}
+      <Header></Header>
       {/** Nav */}
       {/** Results */}
     </div>
